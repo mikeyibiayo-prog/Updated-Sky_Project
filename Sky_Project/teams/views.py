3dc0975f39a77c42395af8d346e2df400e413671
w2091820 <w2091820@westminster.ac.uk>
2026-04-30 05:39:28 +0100

    small fixes

--- Sky_Project/teams/views.py
+++ Sky_Project/teams/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
+@login_required
 def team_directory(request):
     return render(request, 'teams/directory.html')
 
+@login_required
 def team_profile(request):
     return render(request, 'teams/profile.html')
